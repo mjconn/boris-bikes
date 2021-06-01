a0db1417e7243a737264507c4bc1487fcc4b0d0c
Mike Connolly <mjc86@hotmail.co.uk>
2021-06-01 17:10:36 +0100

    end of unit 7

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # boris-bikes
+
+A program that will emulate all the docking stations, bikes, and infrastructure for the Boris Bike scheme.
---
+++ spec/boris-bikes_spec.rb
@@ -0,0 +1,8 @@
+require 'boris-bikes'
+
+describe DockingStation do
+  # it 'release a bike from docking station' do
+  #   docking_station = DockingStation.new
+  #   expect(docking_station).respond_to?(release_bike)
+  # end
+end
